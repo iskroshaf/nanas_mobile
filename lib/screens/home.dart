0669aa46912566388d931b97f095cae8421fc94f
Isk <142883272+iskroshaf@users.noreply.github.com>
2025-11-23 13:16:38 +0800

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 // lib/screens/home.dart
 
 import 'package:flutter/material.dart';
+import 'package:nanas_mobile/styles/sizes.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -12,6 +13,38 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: Center(child: Text('Home'))));
+    final int itemData = 50;
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: kPaddingBody,
+          child: ListView.separated(
+            separatorBuilder: (context, index) => const SizedBox(height: 10),
+            itemCount: itemData,
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  if (index == 0) SizedBox(height: 10),
+                  Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: kBorderRadiusMedium,
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Home ${index + 1}',
+                        style: TextStyle(color: Color(0xffffffff)),
+                      ),
+                    ),
+                  ),
+                  if (index == itemData - 1) SizedBox(height: 10),
+                ],
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
